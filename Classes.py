530fbc5b3741107567571ac9c0dc02e5aa1b013f
Ali Abdelmotalib <ali.abdelmotalib@student-cs.fr>
2020-12-22 22:46:46 +0100

    Adding structure of trees (and nodes) + path decomposition

---
+++ Classes.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, index, value=''):
+        self.index=index
+        self.value=value #string
+        self.parent=None
+        self.children=[]#list of Node objects
+
+    def __str__(self):
+        string=''
+        if self.parent is not None:
+            string+=str(self.parent)+'\n|\n'
+        string+=str((self.index))+'\n|\n'
+        for child in self.children:
+            string+=str(child.index)+' '
+        return string
+
+    def add_children(self,list_nodes):
+        if self.children is None:
+            self.children=list_nodes
+        else:
+            self.children.extend(list_nodes)
+
+    def add_parent(self,node):
+        self.parent=node
+
+    def delete_edge(self,node):
+        if node==self.parent:
+            self.parent=None
+        else:
+            self.children.remove(node)
+class Tree:
+    def __init__(self,root,list_nodes):
+        self.list=list_nodes
+        self.root=root
+        self.index_list=[node.index for node in self.list] 
+        self.slot=max(self.index_list)+1
+
+    def add_edge(self,parent,child):
+        parent.add_children([child])
+        child.add_parent(parent)
+
+    def add_node(self, node):
+        node.index=self.slot
+        self.slot+=1
+        self.list.append(node)
+        self.index_list.append(node.index)
+
+    def delete_node(self,node):
+        (node.parent).delete_edge(node)
+        for child in node.children:
+            child.delete_edge(node)
+        self.list.remove(node)
+        self.index_list.remove(node.index)
+
+    def delete_edge(self,u,v):
+        u.delete_edge(v)
+        v.delete_edge(u)
+
+    def __str__(self):
+        def represent(t,list_of_lists,number):
+            if not any(list_of_lists):
+                return ''
+            else:
+                next=[]
+                result=''
+                for group in list_of_lists:
+                    if any(group):
+                        level=[]
+                        for node in group:
+                            node.index=number
+                            number+=1
+                            level.append(str(node.index))
+                            next+=[node.children]
+                        result+="-".join(level)
+                    result+="|"
+                result+="\n"
+                return result+represent(t,next,number)
+
+        return represent(self,[[self.root]],0)+"\n\n"+"   |||   ".join([str(node.index)+":"+str(node.value) for node in self.list])
---
+++ Path_decomposition.py
@@ -0,0 +1,67 @@
+from Classes import Node, Tree
+
+def sym_difference(node1,node2):
+    sym_diff=[]
+    for char in node1.value:
+        if char not in node2.value:
+            sym_diff.append(char)
+    return sym_diff
+
+def canonical_path(parent,child,tree):
+    starting_sym_diff=sym_difference(parent,child)
+    arrival_sym_diff=sym_difference(child,parent)
+    tree.delete_edge(parent,child)
+    string=parent.value
+    former_node=parent
+    for char in starting_sym_diff:
+        string=string.replace(char,"")
+        actual_node=Node(0,string)
+        former_node.add_children([actual_node])
+        actual_node.add_parent(former_node)
+        tree.add_node(actual_node)
+        former_node=actual_node
+    if len(arrival_sym_diff)>1:
+        arrival_sym_diff=arrival_sym_diff[:-1]
+    for char in arrival_sym_diff:
+        string=string+char
+        actual_node=Node(0,string)
+        former_node.add_children([actual_node])
+        actual_node.add_parent(former_node)
+        tree.add_node(actual_node)
+        former_node=actual_node
+    child.add_parent(former_node)
+    former_node.add_children([child])
+
+def nice_path(path):#type(path)==tree
+    if path.root.value!="":
+        empty_node=Node(0,"")
+        empty_node.add_children([path.root])
+        path.root.add_parent(empty_node)
+        path.add_node(empty_node)
+        path.root=empty_node
+    
+    actual_node=path.root
+    while actual_node.children!=[]:
+        former_node=actual_node
+        actual_node=former_node.children[0] #there is only a child
+        canonical_path(former_node,actual_node,path)
+    if actual_node.value!="":
+        former_node=actual_node
+        empty_node=Node(0,"")
+        former_node.add_children([empty_node])
+        empty_node.add_parent(former_node)
+        path.add_node(empty_node)
+        canonical_path(former_node,empty_node,path)
+
+a=Node(0,"")
+b=Node(0,"abcdefgh")
+a.add_children([b])
+b.add_parent(a)
+t=Tree(a,[a,b])
+print(t)
+
+print("\n next\n")
+
+nice_path(t)
+
+print(t)
