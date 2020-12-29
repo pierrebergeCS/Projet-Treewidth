9c90b8fde28d7e9d0cc82dca33fff58b2002de36
Ali Abdelmotalib <ali.abdelmotalib@student-cs.fr>
2020-12-29 02:04:55 +0100

    Subsequent changes to Tree_decomp

--- Tree_decomposition.py
+++ Tree_decomposition.py
@@ -2,40 +2,27 @@ from Classes import Node, Tree
 from Path_decomposition import canonical_path
 
 def join_subtree(parent, children,tree,flag):#flag is here just for us to know if we still need to remove the edges between the "original" children and their parent
-    children=children[:]    #ne marche pas sinon
+    children=children[:]    #ne marche pas sinon ; deepcopy ne marche pas 
     if not flag:
         flag=True
         for child in children:
-            tree.delete_edge(parent,child)
+            parent.delete_edge(child)
     if len(children)==1:
         parent.add_children(children)
-        children[0].add_parent(parent)
-        tree.add_node(parent)
     else:
         #left
         if children[0].value!=parent.value:
-            parent_left=Node(0,parent.value)
-
-            parent.add_children([parent_left])
+            parent_left=Node(parent.value)
             parent_left.add_parent(parent)
-
-            parent_left.add_children([children[0]])
             children[0].add_parent(parent_left)
-            tree.add_node(parent_left)
         else:
-            parent.add_children([children[0]])
             children[0].add_parent(parent)
             
         #right
-        parent_right=Node(0,parent.value)
-
-        parent.add_children([parent_right])
+        parent_right=Node(parent.value)
         parent_right.add_parent(parent)
-        tree.add_node(parent_right)
         
         join_subtree(parent_right,children[1:],tree,flag)
-
-
 
 def adding_join_subtree(tree, parent, children):
     if any(children):
@@ -52,15 +39,13 @@ def add_leaves(tree,node):
             add_leaves(tree,child)
     else:
         if node.value!='':
-            empty_node=Node(0,'')
-            tree.add_node(empty_node)
-            tree.add_edge(node,empty_node)
+            empty_node=Node('')
+            empty_node.add_parent(node)
         
 def adding_empty_nodes(tree):
     if tree.root.value!='':
-        empty_node=Node(0,"")
-        tree.add_node(empty_node)
-        tree.add_edge(empty_node,tree.root)
+        empty_node=Node("")
+        tree.root.add_parent(empty_node)
         tree.root=empty_node
     add_leaves(tree, tree.root)
 
@@ -71,13 +56,11 @@ def adding_canonical_paths(tree,parent,children):
             adding_canonical_paths(tree,child,child.children)
     elif len(children)==1:
         child=children[0]
-        canonical_path(parent,child,tree)
+        canonical_path(tree,parent,child)
         adding_canonical_paths(tree,child,child.children)
 
 def canonical_tree(tree):
     adding_join_subtree(tree,tree.root,tree.root.children)
     #print(tree)
     adding_empty_nodes(tree)
-    #print(tree)
     adding_canonical_paths(tree,tree.root,tree.root.children)
-    print(tree)
