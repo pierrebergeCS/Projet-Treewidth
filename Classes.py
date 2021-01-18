7955b37ed5a1aa76eac9c7e795907759f2baf085
Ali Abdelmotalib <ali.abdelmotalib@student-cs.fr>
2021-01-18 22:39:44 +0100

    adding width and size

--- Classes.py
+++ Classes.py
@@ -10,10 +10,7 @@ class Node:
     def add_children(self,list_children):
         '''takes a list of nodes as input 
         and make them children of the node self'''
-        if self.children is None:
-            self.children=list_children
-        else:
-            self.children.extend(list_children)
+        self.children.extend(list_children)
         #making self a parent to every node in list_nodes
         for child in list_children:
             child.parent=self
@@ -56,7 +53,7 @@ class Tree:
         means nodes 1 and 2 are both children of 0, 3 is the child of 1
         and 4 and 5 are children of 2
          '''
-        def represent(t,list_of_lists):
+        def represent(list_of_lists):
             '''aux function that use BFS to print all nodes on a "level" 
             and calls itself on the next one '''
             if any(list_of_lists): #if the level is not empty
@@ -74,9 +71,39 @@ class Tree:
                         result+="-".join(level) #children of the same node are separated by '-'
                     result+="|" #separates children of two differents nodes
                 result+="\n" 
-                return result+represent(t,next) # to go from the actual level to the next one
+                return result+represent(next) # to go from the actual level to the next one
 
             else: # checks if the level is empty to end recursion
                 return ''
 
-        return represent(self,[[self.root]])
+        return represent([[self.root]])
+    
+    def width(self):
+        def bfs(level):
+            '''aux function that use BFS to computes the largest width on a level
+            and calls itself on the next one '''
+            if any(level):
+                next=[]
+                result=0
+                for node in level: 
+                    result=max(result,len(node.value))
+                    next.extend(node.children)
+                return max(result,bfs(next))  
+            else:
+                return 0
+        return bfs([self.root])-1 #reminder: width=cardinal of a X -1
+    
+    def size(self):
+        def bfs(level):
+            '''aux function that use BFS to computes the number of nodes on a level
+            and calls itself on the next one '''
+            if any(level):
+                next=[]
+                result=0
+                for node in level: 
+                    result+=1
+                    next.extend(node.children)
+                return result+bfs(next)  
+            else:
+                return 0
+        return bfs([self.root])
