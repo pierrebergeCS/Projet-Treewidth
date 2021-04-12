49916b97e2d3ef5c4f358649be9f355212a0031e
walux1999 <walid-asta@hotmail.com>
2021-04-12 09:59:25 +0200

    Update Elimination_ordering.py

--- Elimination_ordering.py
+++ Elimination_ordering.py
@@ -1,6 +1,7 @@
 from Verification import *
 import copy
 
+
 def eliminate(u, g):
     '''
 
@@ -15,8 +16,9 @@ def eliminate(u, g):
                 g[w].add(v)
         if u in g[v]:
             g[v].remove(u)
-    del(g[u])
+    del (g[u])
     return
+
 
 def sub_tree(l, bag_of, g):
     '''
@@ -33,20 +35,21 @@ def sub_tree(l, bag_of, g):
         return Tree(bag_of[l[0]])
 
     u = l[0]
-    v = l[1] # v is the neighbor of 'u' that has the lowest order in 'l'
-    u_bag = Node([u]+list(g[u]))
+    v = l[1]  # v is the neighbor of 'u' that has the lowest order in 'l'
+    u_bag = Node([u] + list(g[u]))
     bag_of[u] = u_bag
     for w in l:
         if w in g[u]:
-            v = w # here, we extract the said neighbor
+            v = w  # here, we extract the said neighbor
             break
 
-    eliminate(u,g)
+    eliminate(u, g)
     t = sub_tree(l[1:], bag_of, g)
 
     v_bag = bag_of[v]
-    v_bag.add_children([u_bag]) # linking the bags of 'u' and 'v'
+    v_bag.add_children([u_bag])  # linking the bags of 'u' and 'v'
     return t
+
 
 def tree_decomposition_by_elimination(elim_ordering, graph):
     '''
@@ -57,6 +60,7 @@ def tree_decomposition_by_elimination(elim_ordering, graph):
     '''
     g = copy.deepcopy(graph)
     return sub_tree(elim_ordering, {}, g)
+
 
 '''
 graph = {}
@@ -90,7 +94,7 @@ graph['3'] = {'2'}
 
 print(tree_decomposition_by_elimination(['1', '2', '3'], graph))
 print(tree_decomposition_by_elimination(['2', '3', '1'], graph))
-'''
+
 graph = {}
 graph['7'] = {'4', '8'}
 graph['8'] = {'7', '4', '6', '9', '2'}
@@ -103,3 +107,59 @@ graph['3'] = {'2', '6'}
 
 print(tree_decomposition_by_elimination(['1', '3', '7', '9', '4', '6', '2', '8'], graph))
 print(tree_decomposition_by_elimination(['1', '2', '3', '4', '6', '7', '8', '9'], graph))
+'''
+
+
+def greedy_elim(f, graph):
+    '''
+
+    :param f: the greedy function used to select the next element during the construction of an elimination ordering of 'graph'
+    :param graph: a graph represented using a dictionary of sets
+    :return: Constructs a tree decomposition of 'g' following a greedy elimination process imposed by the function 'f'
+    '''
+
+    g = copy.deepcopy(graph)
+    n = len(g)
+    elim_ordering = []
+
+    for i in range(n):
+        u = next(iter(g))
+        for w in g:
+            if f(w, g) < f(u, g):
+                u = w
+        elim_ordering.append(u)
+        eliminate(u, g)
+
+    return elim_ordering
+
+
+def degree(u, g):
+    return len(g[u])
+
+
+def fillin(u, g):
+    '''
+
+        :param u: a node of 'g'
+        :param g: a graph represented using a dictionary of sets
+        :return: The number of missing edges for the neighbourhood of 'u' to be a clique in 'g'
+    '''
+    c = 0
+    for v in g[u]:
+        for w in g[u]:
+            c += (w not in g[v])
+    return c / 2
+
+def deg_fillin(u, g):
+    return fillin(u, g) + degree(u, g)
+
+def sparsity(u, g):
+    return fillin(u, g) - degree(u, g)
+
+def tiebreaker_deg(u, g):
+    n = len(g)
+    return degree(u, g) + (float)fillin(u, g)/(n*n)
+
+def tiebreaker_fillin(u, g):
+    n = len(g)
+    return fillin(u, g) + (float)degree(u, g)/n
